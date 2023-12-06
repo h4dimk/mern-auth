7b9d96e940a781d500dc4ff193d6b10b280fdb90
Hadi mk <hadimk04@gmail.com>
2023-12-06 21:41:18 +0530

    Sign-Up

---
+++ api/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+
+
+mongoose
+  .connect("mongodb://localhost:27017/")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+function SignIn() {
+  return (
+    <div>
+      <h1>SignIn</h1>
+      
+    </div>
+  )
+}
+
+export default SignIn
