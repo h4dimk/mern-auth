a1b2cce0d5e2b4287e76250ab9f0d2128fb4b004
Hadi mk <hadimk04@gmail.com>
2023-12-09 13:28:05 +0530

    update header and made Profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -11,9 +12,11 @@ function App() {
     <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute/>}>
+        <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,29 @@
-import { Link } from "react-router-dom"
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div className='bg-slate-200'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-            <Link to='/'>
-        <h1 className='font-bold'>Auth app</h1>
+    <div className="bg-slate-200">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold">Auth app</h1>
         </Link>
-        <ul className='flex gap-4'>
-            <Link to='/'>
+        <ul className="flex gap-4">
+          <Link to="/">
             <li>Home </li>
-            </Link>
-            <Link to='/sign-in'>
-            <li>Sign In </li>
-            </Link>
+          </Link>
+          <Link to="/profile">
+            {currentUser ? (
+              <img src={currentUser.profilePicture} alt="profile" className="h-7 w-7 rounded-full object-cover " />
+            ) : (
+              <li>Sign In </li>
+            )}
+          </Link>
         </ul>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
