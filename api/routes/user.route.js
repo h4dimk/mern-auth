76d7c63ba001a1ad6728804b0aaeb944c41ea524
Hadi mk <hadimk04@gmail.com>
2023-12-11 12:58:02 +0530

    complete user route api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import { verifyToken } from '../utils/verifyUser.js';
+import { updateUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/',(req,res)=>{
     res.send({ message:"hey"})
 })
+router.post('/update/:id',verifyToken,updateUser)
 
 
 
