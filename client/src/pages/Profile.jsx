10317a56698c2d79887f6623caeeedd7130212b5
Hadi mk <hadimk04@gmail.com>
2023-12-12 16:23:18 +0530

    complete user update functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -20,11 +20,11 @@ function Profile() {
   const [imagePerc, setImagePerc] = useState(0);
   const [imageError, setImageError] = useState(false);
   const [formData, setFormData] = useState({});
-  const { currentUser } = useSelector((state) => state.user);
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  //   allow read;
-  // allow write: if
-  // request. resource.size < 2 * 1024 * 1024 && request.resource.contentType.matches('image/.*')
+
   useEffect(() => {
     if (image) {
       handleFileUpload(image);
@@ -78,6 +78,7 @@ function Profile() {
         dispatch(updateUserFailure(data));
       }
       dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
     } catch (error) {
       dispatch(updateUserFailure(error));
     }
@@ -140,13 +141,17 @@ function Profile() {
           onChange={handleChange}
         />
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Update
+          {loading ? "Loading..." : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
+      <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
+      <p className="text-green-700 mt-5">
+        {updateSuccess && "User is updated successfully!"}
+      </p>
     </div>
   );
 }
