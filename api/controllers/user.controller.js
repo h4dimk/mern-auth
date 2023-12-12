1d0116900ec82c478f7a5ac2c0e29901b8c54cca
Hadi mk <hadimk04@gmail.com>
2023-12-12 19:01:27 +0530

    add signout user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -31,19 +31,15 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
-export const deleteUser = async(req, res, next) => {
-  console.log(req.user.id); 
-  console.log(req.params.id); 
-
-
+export const deleteUser = async (req, res, next) => {
   if (req.user.id != req.params.id) {
     return next(errorHandler(401, "You can delete only your account!"));
   }
 
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.status(200).json('User has been deleted...');
+    res.status(200).json("User has been deleted...");
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,6 +15,7 @@ import {
   deleteUserStart,
   deleteUserFailure,
   deleteUserSuccess,
+  signOut,
 } from "../redux/user/userSlice.js";
 
 function Profile() {
@@ -105,6 +106,17 @@ function Profile() {
       dispatch(deleteUserFailure(error));
     }
   };
+
+  const handleSignOut = async () => {
+    try {
+      await fetch("http://localhost:3000/api/auth/signout",{
+        credentials: "include"
+      });
+      dispatch(signOut());
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7 ">Profile</h1>
@@ -173,7 +185,9 @@ function Profile() {
         >
           Delete Account
         </span>
-        <span className="text-red-700 cursor-pointer">Sign Out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          Sign Out
+        </span>
       </div>
       <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
       <p className="text-green-700 mt-5">
